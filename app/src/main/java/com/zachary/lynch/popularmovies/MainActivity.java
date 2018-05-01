69ce90c8f36b434840697b043ac589470ab937b1
Zach <zachary.lynch1337@gmail.com>
2018-04-30 18:11:00 -0800

    Built url for api and used OkHTTP to connect to movieDB. Also created a method to check if the connection is available or not in order to keep the app from crashing when there is no network.

--- app/src/main/java/com/zachary/lynch/popularmovies/MainActivity.java
+++ app/src/main/java/com/zachary/lynch/popularmovies/MainActivity.java
@@ -1,13 +1,67 @@
 package com.zachary.lynch.popularmovies;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // check if the network is available
+        ApiKey apiKey = new ApiKey();
+        String movieUrl = "https://api.themoviedb.org/3/discover/movie?api_key=" +
+                apiKey + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
+        if (isNetworkAvailable()){
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().
+                    url(movieUrl)
+                    .build();
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                        }
+                    });
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+
+                }
+            });
+
+        } else {
+            Toast.makeText(this, "Network unavailable", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager)
+                getSystemService(getApplicationContext().CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if (networkInfo != null && networkInfo.isConnected()){
+            isAvailable = true;
+        }
+        return isAvailable;
     }
 }
